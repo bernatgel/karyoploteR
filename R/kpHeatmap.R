c9d6311bbb93d58d43782f526cbd53288b8279e1
Bernat Gel <bgel@imppc.org>
2015-10-19 11:08:57 +0200

    kpHeatmap added to plot heatmaps over the genome

--- R/kpHeatmap.R
+++ R/kpHeatmap.R
@@ -1,7 +1,7 @@
 
 
 
-kpRect <- function(karyoplot, data=NULL, chr=NULL, x0=NULL, x1=x0, ymax=NULL, ymin=NULL, data.panel=1, bar.width=NULL, ...) {
+kpHeatmap <- function(karyoplot, data=NULL, chr=NULL, x0=NULL, x1=x0, y=NULL, vmax=NULL, vmin=NULL, data.panel=1, colors=c("blue", "white", "yellow"), ...) {
   if(!is(karyoplot, "KaryoPlot")) stop("'karyoplot' must be a valid 'KaryoPlot' object")
   
   ccf <- karyoplot$coord.change.function
@@ -11,38 +11,40 @@ kpRect <- function(karyoplot, data=NULL, chr=NULL, x0=NULL, x1=x0, ymax=NULL, ym
     x0 <- start(data)
     x1 <- end(data)
         
-    if(is.null(ymax)) {
-      if("ymax" %in% names(mcols(data))) {
-        ymax <- data$ymax
+    if(is.null(y)) {
+      if("value" %in% names(mcols(data))) {
+        y <- data$value
       } else {
-        stop("No ymax value specified. It is needed to provide ymax or a column named 'ymax' in data")
-      }
-    }
-    if(is.null(ymin)) {
-      if("ymin" %in% names(mcols(data))) {
-        ymin <- data$ymin
-      } else {
-        stop("No ymin value specified. It is needed to provide ymin or a column named 'ymin' in data")
+        if("y" %in% names(mcols(data))) {
+          y <- data$y
+        } else {
+          stop("No y value specified. It is needed to provide ymax or a column named 'y' in data")
+        }
       }
     }
   } 
   
-  OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO PENDENT
+  if(is.null(vmin)) vmin <- min(y)
+  if(is.null(vmax)) vmax <- max(y)
+    
+  #Standardize the values to the [0,1] range to plot it with colorRamp
+  y <- y - vmin
+  y <- y/(vmax - vmin)
   
-#   my.colors<-colorRampPalette(c("blue", "white", "red")) #creates a function my.colors which interpolates n colors between blue, white and red
-#   color.df<-data.frame(COLOR_VALUE=seq(-1000,1000,1), color.name=my.colors(2001)) #generates 2001 colors from the color ramp
-#   reg1.with.color<-merge(reg1, color.df, by="COLOR_VALUE")
   
+  #Create the colorRamp
+  cr <- colorRamp(colors=colors)
   
-  #Extend ymin and ymax as necessary
-  ymin <- rep_len(ymin, length(x0))
-  ymax <- rep_len(ymax, length(x0))
+  print(head(y))
+  print(cr(head(y)))
+  
 
+  #Determine the plotting coordinates
   x0plot <- ccf(chr=chr, x=x0, data.panel=data.panel)$x
   x1plot <- ccf(chr=chr, x=x1, data.panel=data.panel)$x
-  yminplot <- ccf(chr=chr, y=ymin, data.panel=data.panel)$y
-  ymaxplot <- ccf(chr=chr, y=ymax, data.panel=data.panel)$y
+  yminplot <- ccf(chr=chr, y=rep_len(0, length(chr)), data.panel=data.panel)$y
+  ymaxplot <- ccf(chr=chr, y=rep_len(1, length(chr)), data.panel=data.panel)$y
   
-  rect(xleft=x0plot, xright=x1plot, ytop=ymaxplot, ybottom=yminplot, ...)
+  rect(xleft=x0plot, xright=x1plot, ytop=ymaxplot, ybottom=yminplot, col=rgb(cr(y), max=255), border=NA, ...)
   
 }
