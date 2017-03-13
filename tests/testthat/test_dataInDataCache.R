2c2ba5b5b172866cdfcb5e06d8630e73c9b78ba1
Bernat Gel <bgel@imppc.org>
2017-03-13 11:11:38 +0100

    Added tests with testthat

---
+++ tests/testthat/test_dataInDataCache.R
@@ -0,0 +1,13 @@
+library(karyoploteR)
+context("Content of the data cache")
+
+#Test order of genomes
+test_that("the genomes are sorted", {
+  dc <- karyoploteR:::data.cache
+  
+  #hg19, canonical chromosomes
+  expect_equal(paste0(seqlevels(dc[["genomes"]][["hg19"]])[1:24], collapse = ","), paste0(paste0("chr", c(1:22, "X", "Y")), collapse = ","))
+  
+  #TODO: Check the other genomes
+  
+})
