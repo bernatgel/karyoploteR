9c7d4c97ba652b143e69d5682e16f990e63c482d
Bernat Gel <bernatgel@gmail.com>
2019-09-04 14:51:56 +0200

    Added more tests on colors.R

--- tests/testthat/test_colors.R
+++ tests/testthat/test_colors.R
@@ -15,7 +15,29 @@ test_that("preprocessColors works correctly", {
   
   
 })    
-    
+  
+
+test_that("darker, lighter and transparent work correctly", {
+  
+  expect_equal(darker(c("red", "#00FF00", NA, "black")), c("#690000", "#006900", NA, "#000000"))
+  expect_equal(darker(c("red", "#00FF00", NA, "black"), amount=20), c("#EB0000", "#00EB00", NA, "#000000"))
+  expect_error(darker(c("red", "#00FF00", NA, "black"), amount=-1), "amount must be a number between 0 and 255")
+  expect_error(darker(c("red", "#00FF00", NA, "black"), amount=FALSE), "amount must be a single number")
+  expect_error(darker(c("red", "#00FF00", NA, "black"), amount="GREEN"), "amount must be a single number")
+  
+  expect_equal(lighter(c("red", "#00FF00", NA, "white")), c("#FF9696", "#96FF96", NA, "#FFFFFF"))
+  expect_equal(lighter(c("red", "#00FF00", NA, "white"), amount=20), c("#FF1414", "#14FF14", NA, "#FFFFFF"))
+  expect_error(lighter(c("red", "#00FF00", NA, "black"), amount=-1), "amount must be a number between 0 and 255")
+  expect_error(lighter(c("red", "#00FF00", NA, "black"), amount=FALSE), "amount must be a single number")
+  expect_error(lighter(c("red", "#00FF00", NA, "black"), amount="GREEN"), "amount must be a single number")
+  
+  expect_equal(transparent(c("red", "#00FF00", NA, "black")), c("#FF000080", "#00FF0080", NA, "#00000080"))
+  expect_equal(transparent(c("red", "#00FF00", NA, "white"), amount=0.2), c("#FF0000CC", "#00FF00CC", NA, "#FFFFFFCC"))
+  expect_error(transparent(c("red", "#00FF00", NA, "black"), amount=-1), "amount must be a number between 0 and 1")
+  expect_error(transparent(c("red", "#00FF00", NA, "black"), amount=FALSE), "amount must be a single number")
+  expect_error(transparent(c("red", "#00FF00", NA, "black"), amount="GREEN"), "amount must be a single number")
+   
+})  
     
 
 
