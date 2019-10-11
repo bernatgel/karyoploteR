0db18ee6440e88d1d9496fd44006afa30715ce77
Bernat Gel <bernatgel@gmail.com>
2019-10-11 16:15:27 +0200

    Added support for VCF's in kpPlotRainfall

--- R/kpPlotRainfall.R
+++ R/kpPlotRainfall.R
@@ -15,11 +15,13 @@
 #' 
 #' There's more information at the \url{https://bernatgel.github.io/karyoploter_tutorial/}{karyoploteR tutorial}.
 #' 
-#' @usage kpPlotRainfall(karyoplot, data=NULL, col=NULL, ymin=NULL, ymax=7, data.panel=1, r0=NULL, r1=NULL, clipping=TRUE, ...)
+#' @usage kpPlotRainfall(karyoplot, data, ref=NULL, alt=NULL, col="cell21breast", ymin=NULL, ymax=7, data.panel=1, r0=NULL, r1=NULL, clipping=TRUE, ...)
 #' 
 #' @param karyoplot    (a \code{KaryoPlot} object) This is the first argument to all data plotting functions of \code{karyoploteR}. A KaryoPlot object referring to the currently active plot.
-#' @param data    (a \code{GRanges}) A GRanges object with the features to be plotted.
-#' @param col (a color vector) The colors to use to draw the points. If the length of the vector is lower than the length of data, it will be recycled. If NULL, points will be plotted in black. (defaults to NULL)
+#' @param data    (a \code{GRanges}, a \code{VRanges} or a path to a VCF file) A GRanges or VRanges object with the variants to plot or the path to a VCF file.
+#' @param ref (character vector or NULL) A character vector of with the reference base of the variants in data. Used to determine the color. If NULL and data is a VRanges or a VCF file, the information in data will be used (defaults to NULL)
+#' @param alt (character vector or NULL) A character vector of with the alternative base of the variants in data. Used to determine the color. If NULL and data is a VRanges or a VCF file, the information in data will be used (defaults to NULL)
+#' @param col (a color vector, a color table or a color schema) The colors to use to draw the points. If the length of the vector is lower than the length of data, it will be recycled. Color table and color schema must be compatible with getVariantColors. If NULL, points will be plotted in black. (defaults to NULL)
 #' @param data.panel    (numeric) The identifier of the data panel where the data is to be plotted. The available data panels depend on the plot type selected in the call to \code{\link{plotKaryotype}}. (defaults to 1)
 #' @param r0    (numeric) r0 and r1 define the vertical range of the data panel to be used to draw this plot. They can be used to split the data panel in different vertical ranges (similar to tracks in a genome browser) to plot differents data. If NULL, they are set to the min and max of the data panel, it is, to use all the available space. (defaults to NULL)
 #' @param r1    (numeric) r0 and r1 define the vertical range of the data panel to be used to draw this plot. They can be used to split the data panel in different vertical ranges (similar to tracks in a genome browser) to plot differents data. If NULL, they are set to the min and max of the data panel, it is, to use all the available space. (defaults to NULL)
@@ -46,26 +48,59 @@
 #'  
 #' @export kpPlotRainfall
 
-kpPlotRainfall <- function(karyoplot, data=NULL, col=NULL, ymin=NULL, ymax=7, data.panel=1, r0=NULL, r1=NULL, clipping=TRUE, ...) {
+kpPlotRainfall <- function(karyoplot, data, ref=NULL, alt=NULL, col="cell21breast", ymin=NULL, ymax=7, data.panel=1, r0=NULL, r1=NULL, clipping=TRUE, ...) {
 
   if(!methods::is(karyoplot, "KaryoPlot")) stop(paste0("In kpPlotRainfall: 'karyoplot' must be a valid 'KaryoPlot' object"))
-  if(!methods::is(data, "GRanges")) stop(paste0("In kpPlotRainfall: 'data' must be a valid 'GRanges' object"))
-
-  #TODO: Convert the VCF into a GRanges if needed
+  if(!is.null(ref) & !is.character(ref)) stop("In kpPlotRainfall: ref must be NULL or a character vector")
+  if(!is.null(alt) & !is.character(alt)) stop("In kpPlotRainfall: alt must be NULL or a character vector")
   
-  #Assign colors if needed
-  if(is.null(col)) {
-    col="#000000"
+  #Preprocess data
+  if(is.null(data)) stop(paste0("In kpPlotRainfall: data cannot be NULL."))
+
+  if(!methods::is(data, "GRanges") && !methods::is(data, "VRanges")) {
+    if(is.character(data) && length(data)==1) {
+      data <- tryCatch(readVcfAsVRanges(data), error=function(e) {stop("In kpPlotRainfall: Error when reading the VCF file. Is it a VCF file?")})
+    } else {
+      stop(paste0("In kpPlotRainfall: 'data' must be a valid 'GRanges' object or a VCF file"))     
+    }
   }
-  if(length(col)!=length(data)) {
-    col <- rep(col, len=length(data))
+  
+  if(methods::is(data, "VRanges")) {
+    if(is.null(ref)) ref <- ref(data)
+    if(is.null(alt)) alt <- alt(data)
+    #And convert into a standard GRanges
+    data <- GRanges(data)
+    mcols(data) <- NULL
+  }
+
+  #Remove the indels (if possible)
+  if(!is.null(ref) && !is.null(alt)) {
+    indels <- nchar(ref)!=1 | nchar(alt)!=1
+    data <- data[!indels]
+    ref <- ref[!indels]
+    alt <- alt[!indels]
+  }
+  
+  #Assign colors
+  if(is.null(col)) col <- "black"
+  #If it's a color table
+  if(length(names(col))==7 && all(sort(names(col))==sort(names(.karyoploter.colors$variants$schemas[[1]])))) {
+    if(is.null(ref) || is.null(alt)) stop("In kpPlotRainfall: a color.table cannot be used if ref and alt are NULL. Provide ref and alt or make data a VRanges or a VCF file")
+    col <- getVariantsColors(ref=ref, alt=alt, color.table = col)
+  } else {
+    if(all(is.color(col))) {
+      col <- rep(col, length.out=length(vars))
+    } else { #If it's not a color, assume it's a valid schema name
+      if(is.null(ref) || is.null(alt)) stop("In kpPlotRainfall: a color.schema cannot be used if ref and alt are NULL. Provide ref and alt or make data a VRanges or a VCF file")
+      col <- getVariantsColors(ref=ref, alt=alt, color.schema = col)
+    }
   }
   
   #Sort and split by chromosome
-    #first attach the color to the granges so colors are not misasigned
-    mcols(data) <- data.frame(col=col, stringsAsFactors = FALSE)
-    
+  #first attach the color to the granges so colors are not misasigned
+  data$col <- col
   data <- sort(data)
+  
   data.per.chr <- split(data, seqnames(data))
   
   distances <- list()
@@ -80,7 +115,7 @@ kpPlotRainfall <- function(karyoplot, data=NULL, col=NULL, ymin=NULL, ymax=7, da
     }
   }
   
-  karyoplot$latest.plot <- list(funct="kpPlotRainfall", computed.values=list(distances=distances))
+  karyoplot$latest.plot <- list(funct="kpPlotRainfall", computed.values=list(distances=distances, vars=data, ref=ref, alt=alt))
 
   invisible(karyoplot)
 }
