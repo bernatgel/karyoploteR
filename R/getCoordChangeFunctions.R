35a386f3d63d74baf655fc1f2f1bf0676529fd48
Bernat Gel <bernatgel@gmail.com>
2020-04-08 01:05:21 +0200

    Comment out unused code (vertical ideograms ccf)

--- R/getCoordChangeFunctions.R
+++ R/getCoordChangeFunctions.R
@@ -403,76 +403,76 @@ genomic2plot_3HorizAllChromosomesInOneLine <- function(chr=NULL, x=NULL, y=NULL,
 
 
 
-####################################################################################
-#
-#    Plot Type 4 - Vertical with Data Above and Below the Ideogram  (BEGIN)
-#
-####################################################################################
-
-getIdeogramMidY_4VerticalDataAboveAndBelowIdeogram <- function(chr, genome, plot.params) {
-  pp <- plot.params
-  chr.height <- getChrHeight_4VerticalDataAboveAndBelowIdeogram(pp)
-  chr.names <- GenomeInfoDb::seqlevels(genome)
-  chrs <- c(length(chr.names):1)
-  names(chrs) <- chr.names
-  chr.num <- chrs[chr]
-  return(pp$bottommargin + (chr.num - 1) * chr.height +
-           pp$data2outmargin + pp$data2height + pp$data2inmargin + pp$ideogramheight/2)
-}
-
-getChrHeight_4VerticalDataAboveAndBelowIdeogram <- function(pp) {
-  chr.height <- (pp$data2outmargin + pp$data2height + pp$data2inmargin
-                 + pp$ideogramheight
-                 + pp$data1inmargin + pp$data1height + pp$data1outmargin)
-  return(chr.height)
-}
-
-#Build the function mapping genomic regions into plotting coordinates
-genomic2plot_4VerticalDataAboveAndBelowIdeogram <- function(chr=NULL, x=NULL, y=NULL, 
-                                                            data.panel, genome, plot.params) {
-  
-  pp <- plot.params
-  
-  genome.width <- 1 - pp$leftmargin - pp$rightmargin
-  max.chr.len <- max(end(genome)) - min(start(genome))
-  
-  if(!is.null(x)) {
-    x.plot <- pp$leftmargin + (x/max.chr.len)*genome.width
-  } else{
-    x.plot <- NULL
-  }
-  
-  if(is.null(chr)) {
-    y.plot <- NULL 
-  } else {
-    #chrs.y <- getChrLowestY(chr, genome, pp) 
-    chrs.y <- getIdeogramMidY_4VerticalDataAboveAndBelowIdeogram(chr, genome, pp) 
-    if(is.null(y)) { #if y is null, set y to the bottom of the chromosome
-      y.plot <- chrs.y
-    } else { #Return the appropiate plot.y for the given original y
-      if(data.panel == 1) {
-        datayrange <- pp$data1max - pp$data1min 
-        yscaled <- ((y - pp$data1min) / datayrange) * pp$data1height
-        y.plot <- chrs.y + pp$ideogramheight/2 + pp$data1inmargin + yscaled
-      } else {
-        if(data.panel == 2) {
-          datayrange <- pp$data2max - pp$data2min 
-          yscaled <- ((y - pp$data2min) / datayrange) * pp$data2height
-          y.plot <- chrs.y - pp$ideogramheight/2 - pp$data2inmargin - yscaled
-        } else {
-          stop("Invalid data.panel")
-        }
-      }
-    }
-  }   
-  return(list(x=y.plot, y=x.plot))
-}
-
-
-#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-#
-#    Plot Type 4 - Vertical with Data Above and Below the Ideogram  (END)
-#
-#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# ####################################################################################
+# #
+# #    Plot Type 4 - Vertical with Data Above and Below the Ideogram  (BEGIN)
+# #
+# ####################################################################################
+# 
+# getIdeogramMidY_4VerticalDataAboveAndBelowIdeogram <- function(chr, genome, plot.params) {
+#   pp <- plot.params
+#   chr.height <- getChrHeight_4VerticalDataAboveAndBelowIdeogram(pp)
+#   chr.names <- GenomeInfoDb::seqlevels(genome)
+#   chrs <- c(length(chr.names):1)
+#   names(chrs) <- chr.names
+#   chr.num <- chrs[chr]
+#   return(pp$bottommargin + (chr.num - 1) * chr.height +
+#            pp$data2outmargin + pp$data2height + pp$data2inmargin + pp$ideogramheight/2)
+# }
+# 
+# getChrHeight_4VerticalDataAboveAndBelowIdeogram <- function(pp) {
+#   chr.height <- (pp$data2outmargin + pp$data2height + pp$data2inmargin
+#                  + pp$ideogramheight
+#                  + pp$data1inmargin + pp$data1height + pp$data1outmargin)
+#   return(chr.height)
+# }
+# 
+# #Build the function mapping genomic regions into plotting coordinates
+# genomic2plot_4VerticalDataAboveAndBelowIdeogram <- function(chr=NULL, x=NULL, y=NULL, 
+#                                                             data.panel, genome, plot.params) {
+#   
+#   pp <- plot.params
+#   
+#   genome.width <- 1 - pp$leftmargin - pp$rightmargin
+#   max.chr.len <- max(end(genome)) - min(start(genome))
+#   
+#   if(!is.null(x)) {
+#     x.plot <- pp$leftmargin + (x/max.chr.len)*genome.width
+#   } else{
+#     x.plot <- NULL
+#   }
+#   
+#   if(is.null(chr)) {
+#     y.plot <- NULL 
+#   } else {
+#     #chrs.y <- getChrLowestY(chr, genome, pp) 
+#     chrs.y <- getIdeogramMidY_4VerticalDataAboveAndBelowIdeogram(chr, genome, pp) 
+#     if(is.null(y)) { #if y is null, set y to the bottom of the chromosome
+#       y.plot <- chrs.y
+#     } else { #Return the appropiate plot.y for the given original y
+#       if(data.panel == 1) {
+#         datayrange <- pp$data1max - pp$data1min 
+#         yscaled <- ((y - pp$data1min) / datayrange) * pp$data1height
+#         y.plot <- chrs.y + pp$ideogramheight/2 + pp$data1inmargin + yscaled
+#       } else {
+#         if(data.panel == 2) {
+#           datayrange <- pp$data2max - pp$data2min 
+#           yscaled <- ((y - pp$data2min) / datayrange) * pp$data2height
+#           y.plot <- chrs.y - pp$ideogramheight/2 - pp$data2inmargin - yscaled
+#         } else {
+#           stop("Invalid data.panel")
+#         }
+#       }
+#     }
+#   }   
+#   return(list(x=y.plot, y=x.plot))
+# }
+# 
+# 
+# #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# #
+# #    Plot Type 4 - Vertical with Data Above and Below the Ideogram  (END)
+# #
+# #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 
